bbe93f59d13ab3cb6fdeaccc3e2f3d5edbe425cc
RegularVitalyDEV <hah233399@gmail.com>
2024-09-29 19:40:37 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,4 @@ if not getgenv().ExecutorSupport then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/executorTest.lua"))()
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/places/" .. game.GameId .. ".lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVitalyDEV/lolhaxbutbetter/refs/heads/main/mainhotel.lua/" .. game.GameId .. ".lua"))()
