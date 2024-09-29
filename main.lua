bbad2792e0b226ca16f5683fcb75e70d71dce1b4
RegularVitalyDEV <hah233399@gmail.com>
2024-09-29 19:49:47 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,4 @@ if not getgenv().ExecutorSupport then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVitalyDEV/lolhaxbutbetter/main/mainhotel.lua"))()
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVitalyDEV/lolhaxbutbetter/main/mainhotel.lua" .. game.GameId .. ".lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVitalyDEV/lolhaxbutbetter/main/mainhotel.lua/" .. game.GameId .. ".lua"))()
